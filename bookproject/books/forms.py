78c6cc0b747d5b0ace08eac0bb311014353b936a
Dimitris Kougioumtzis <dimkoug@gmail.com>
2025-04-20 20:44:47 +0300

    wip

--- bookproject/books/forms.py
+++ bookproject/books/forms.py
@@ -61,11 +61,16 @@ class CategoryForm(BootstrapForm, forms.ModelForm):
 class BookForm(BootstrapForm, forms.ModelForm):
     class Meta:
         model = Book
-        fields = ('title', 'category', 'image')
+        fields = ('title', 'category','publisher','isbn','publication_date','language','pages','genre','format',
+                  'edition','summary','copies_available','location','is_available',
+                  'dewey_decimal','library_of_congress','file_url','file_format',
+                  'image')
 
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop("request")
         super().__init__(*args, **kwargs)
+
+
 
 BookFormSet = inlineformset_factory(Category, Book, BookForm, formset=unique_bootstrap_field_formset('name'), can_delete=True)
 BookAuthorFormSet = inlineformset_factory(Book, BookAuthor, fields=('author',),formset=unique_bootstrap_field_formset('author'), can_delete=True)
